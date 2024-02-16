23e576314c3e1e0461c7890c9806875dd171b83e
Alexandre Carvalho <carvalho2209@hotmail.com>
2024-02-15 17:09:53 -0800

    dynamic links

--- src/app/navigation/menu/menu/menu.component.ts
+++ src/app/navigation/menu/menu/menu.component.ts
@@ -5,5 +5,38 @@ import { Component } from '@angular/core';
   templateUrl: './menu.component.html'
 })
 export class MenuComponent {
+  nav: Nav[] = [
+    {
+      link: '/home',
+      name: 'Home',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/about',
+      name: 'About',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/register',
+      name: 'Register',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/products',
+      name: 'Products',
+      exact: true,
+      admin: false
+    },
+  ];
+}
 
+
+interface Nav {
+  link: string,
+  name: string,
+  exact: boolean,
+  admin: boolean
 }
